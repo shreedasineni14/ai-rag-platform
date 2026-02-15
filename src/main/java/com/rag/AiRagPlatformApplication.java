d25dde1dd2fd5713428fe8d67d04a29a019ef06f
shreedasineni14 <shreedasineni14@gmail.com>
2026-02-15 14:43:26 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/rag/AiRagPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRagPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRagPlatformApplication.class, args);
+	}
+
+}
